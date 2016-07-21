a136bf9947482dfddf3552601eaf72bc65cde2dd
Kennedy Oliveira <kennedy.oliveira@outlook.com>
2016-07-21 11:23:25 -0300

    Adicionando método utilitario na proposta de acordo para recuperar o status da validação

--- src/main/java/br/com/catelani/santander/filetransfer/domain/PropostaAcordo.java
+++ src/main/java/br/com/catelani/santander/filetransfer/domain/PropostaAcordo.java
@@ -24,5 +24,15 @@ public class PropostaAcordo {
    * Proposta de acordos.
    */
   private List<DetalhePropostaAcordo> detalhes;
+
+  /**
+   * <p>Método para facil recuperação do Código de Retorno do Banco para essa proposta.</p>
+   * <p>Esse método é a mesma coisa que chamar o {@link RetornoFinanceiraPA#getCodigoRetornoValidacao()}.</p>
+   *
+   * @return O Código de Retorno do Banco com o status dessa proposta.
+   */
+  public CodigoRetornoValidacao getCodigoRetornoValidacao() {
+    return cabecalho != null && cabecalho.getRetornoFinanceira() != null ? cabecalho.getRetornoFinanceira().getCodigoRetornoValidacao() : null;
+  }
 }
 
