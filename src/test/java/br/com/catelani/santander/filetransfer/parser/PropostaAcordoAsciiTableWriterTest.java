5cf9822a2fe08eac008b53aa632754c39d4c1f4a
Kennedy Oliveira <kennedy.oliveira@outlook.com>
2016-07-25 14:47:43 -0300

    corrigindo tests e utilizando parameterized tests para melhorar a legibilidade

--- src/test/java/br/com/catelani/santander/filetransfer/parser/PropostaAcordoAsciiTableWriterTest.java
+++ src/test/java/br/com/catelani/santander/filetransfer/parser/PropostaAcordoAsciiTableWriterTest.java
@@ -5,14 +5,23 @@ import br.com.catelani.santander.filetransfer.qualifiers.AsciiTable;
 import com.google.inject.Guice;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import javax.inject.Inject;
 import java.io.InputStream;
+import java.util.Arrays;
 
 /**
  * @author Kennedy Oliveira
  */
+@RunWith(Parameterized.class)
 public class PropostaAcordoAsciiTableWriterTest {
+
+  @Parameter
+  public String caminhoArquivoProposta;
 
   @Inject
   @AsciiTable
@@ -21,32 +30,19 @@ public class PropostaAcordoAsciiTableWriterTest {
   @Inject
   private PropostaAcordoReader propostaAcordoReader;
 
+  @Parameters(name = "Gerando AsciiTable com arquivo \"{0}\"")
+  public static Iterable<? extends String> data() {
+    return Arrays.asList("exemplos/hf/erro/ESCPA0036.DAT", "exemplos/hf/erro/ESCRN0036.DAT", "exemplos/hf/erro/ESCPR0036.DAT");
+  }
+
   @Before
   public void setUp() throws Exception {
     Guice.createInjector(new FileTransferModule()).injectMembers(this);
   }
 
   @Test
-  public void testGerarAsciiTableRetornoComErro() throws Exception {
-    final InputStream arquivoProposta = ClassLoader.getSystemResourceAsStream("exemplos/hf/erro/ESCRN0036.dat");
-
-    final PropostaAcordo propostaAcordo = propostaAcordoReader.parse(arquivoProposta);
-
-    propostaAcordoWriter.gerarPropostaAcordo(propostaAcordo, System.out);
-  }
-
-  @Test
-  public void testGerarAsciiTablePropostaAcordo() throws Exception {
-    final InputStream arquivoProposta = ClassLoader.getSystemResourceAsStream("exemplos/hf/erro/ESCPA0036.dat");
-
-    final PropostaAcordo propostaAcordo = propostaAcordoReader.parse(arquivoProposta);
-
-    propostaAcordoWriter.gerarPropostaAcordo(propostaAcordo, System.out);
-  }
-
-  @Test
-  public void testGerarAsciiTableProtocoloRecebimento() throws Exception {
-    final InputStream arquivoProposta = ClassLoader.getSystemResourceAsStream("exemplos/hf/erro/ESCPR0036.dat");
+  public void testGerarAsciiTable() throws Exception {
+    final InputStream arquivoProposta = ClassLoader.getSystemResourceAsStream(caminhoArquivoProposta);
 
     final PropostaAcordo propostaAcordo = propostaAcordoReader.parse(arquivoProposta);
 
